aa71421b0f0cbef4256140c2596dae4a0e281ca0
agilasadi <newsma.95@gmail.com>
2019-08-15 16:13:31 +0300

    composer.json was updated

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Rapkit\\Wire\\' => array($baseDir . '/packages/Rapkit/Wire/src'),
+    'App\\' => array($baseDir . '/app'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,33 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitfeeb09d7668ca3d20d98e0da2c239941
 {
+    public static $prefixLengthsPsr4 = array (
+        'R' => 
+        array (
+            'Rapkit\\Wire\\' => 12,
+        ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Rapkit\\Wire\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/packages/Rapkit/Wire/src',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitfeeb09d7668ca3d20d98e0da2c239941::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitfeeb09d7668ca3d20d98e0da2c239941::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
