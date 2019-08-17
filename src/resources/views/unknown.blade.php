28aaf56b9a13646736e79f1f8b9191226ad56c0e
agilasadi <newsma.95@gmail.com>
2019-08-17 22:24:46 +0300

    unknown view was added, it will be returned when an undefined identifier was given

---
+++ src/resources/views/unknown.blade.php
@@ -0,0 +1,16 @@
+@extends('wire.views.layouts.admin_master')
+
+@section('content')
+<div class="container-fluid">
+    <section class="py-5">
+      <div class="container text-center">
+        <h1 class="display-1">404</h1>
+        <p class="lead"><b class="text-capitalize h6">{{ $parameter}}</b> was not found in the identifier list.</p>
+        <div class="row">
+          <p class="col-md-6 mx-auto">As well as funds, willing VC-s, client-centered approach, basic UX skills, proper level of nutrients, girlfriends and most of our dignity.</p>
+        </div>
+      </div>
+    </section>
+  </div>
+@endsection
+
