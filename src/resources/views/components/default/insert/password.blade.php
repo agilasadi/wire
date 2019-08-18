a407dde7dc576ae04f70c076a72832e1c9cab52f
agilasadi <newsma.95@gmail.com>
2019-08-18 12:27:27 +0300

    Password field, table and form component was added in

---
+++ src/resources/views/components/default/insert/password.blade.php
@@ -0,0 +1,13 @@
+<div class="form-group">
+    <label class="text-capitalize" for="{{ $key }}">{{ str_replace("_", " ", $key) }}</label>
+
+    <input type="password" class="form-control" id="{{ $key }}" name="{{ $key }}"
+        placeholder="{{ str_replace("_", " ", $key) }}"
+        value="{{ old($key) ? old($key) : "" }}">
+
+    @if ($errors->has($key))
+        <div class="invalid-feedback d-block">
+            {{ $errors->first($key) }}
+        </div>
+    @endif
+</div>
