53a35aac8ae4ded146b355245e8277167b7e3874
agilasadi <newsma.95@gmail.com>
2019-08-19 13:06:58 +0300

    Config file was updated to have guard and authentication parameters

--- src/config/wire.php
+++ src/config/wire.php
@@ -1,21 +1,52 @@
 <?php
 
 return [
-    /*
-     * --------------------------------------------------------------------------------------------
-     *  Wire Application Name
-     * --------------------------------------------------------------------------------------------
-     *
-     * This name is used to be shown everywhere in wire interface.
-     */
-    'name' => 'Wire',
+	/*
+	 * --------------------------------------------------------------------------------------------
+	 *  Wire Application Name
+	 * --------------------------------------------------------------------------------------------
+	 *
+	 * This name is used to be shown everywhere in wire interface.
+	 */
+	'name' => 'Wire',
 
-    /*
-     * --------------------------------------------------------------------------------------------
-     *  Model Path
-     * --------------------------------------------------------------------------------------------
-     *
-     * Allows Wire to automatically discover available Model when an identifier is created
-     */
-    'model_path' => 'App\\',
+	/*
+	 * --------------------------------------------------------------------------------------------
+	 *  Model Path
+	 * --------------------------------------------------------------------------------------------
+	 *
+	 * Allows Wire to automatically discover available Model when an identifier is created
+	 */
+	'model_path' => 'App\\',
+
+	/*
+	 * --------------------------------------------------------------------------------------------
+	 *  Authenticate With
+	 * --------------------------------------------------------------------------------------------
+	 *
+	 * The parameters in authenticate_with are used to log user in with the given parameters.
+	 *
+	 * Key in the parameters are used as database columns, and the value is the type of the
+	 * parameter. Currently you can have two types of columns. Either string such as
+	 * username or email which in that case the field will be checked to see if it satisfies
+	 * parameters of an email address.
+	 */
+	'authenticate_with' => [
+		"slug" => "string",
+		'email' => "email"
+	],
+
+
+	/*
+	 * --------------------------------------------------------------------------------------------
+	 *  Guard
+	 * --------------------------------------------------------------------------------------------
+	 *
+	 * You can modify wire_guard field to protect access to the Admin Panel vie your custom guard.
+	 * By default laravel has web, and api guards.
+	 *
+	 * You can add guards in auth.php
+	 * For more information visit laravel.com
+	 */
+	'wire_guard' => 'web',
 ];
