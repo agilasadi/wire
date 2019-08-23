ad1e3f2035a9a82ebfa7afd523ea7ae08dadec44
agilasadi <newsma.95@gmail.com>
2019-08-23 11:34:11 +0300

    image component was updated to have alternative of wire

--- src/resources/views/components/default/table/image.blade.php
+++ src/resources/views/components/default/table/image.blade.php
@@ -5,10 +5,10 @@
 	     @else
 	     src="{{asset('storage/'.$record[$key])}}"
 	     @endif
-	     style="width: {{ @$width ? $width : '1.5rem' }}; height: {{ @$height ? $height : '1.5rem' }}" alt="asarsoft">
+	     style="width: {{ @$width ? $width : '1.5rem' }}; height: {{ @$height ? $height : '1.5rem' }}" alt="wire">
 @else
 	<img class="{{ @$class }} rounded-circle"
 	     src="{{asset('wire-assets/wire-images/placeholder/no_image.jpg')}}"
-	     style="width: {{@$width ? $width : '1.5rem'}}; height: {{ @$height ? $height : '1.5rem' }}" alt="asarsoft">
+	     style="width: {{@$width ? $width : '1.5rem'}}; height: {{ @$height ? $height : '1.5rem' }}" alt="wire">
 @endif
 {{ $slot }}
