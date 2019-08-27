fec690e0d860c9d1ad934542f159e9513b85ac8b
agilasadi <newsma.95@gmail.com>
2019-08-27 12:03:55 +0300

    Wire guide was uploaded to help new comers learn how to initiate identifiers

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -2,6 +2,9 @@
 @section('content')
 
 @include('wire.views.includes.statistics')
+
+@include('wire.views.includes.wire-guide')
+
     <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
 
     <h2>Section title</h2>
