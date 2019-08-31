3febc801fac75895595c755ffba035611d262302
agilasadi <newsma.95@gmail.com>
2019-08-31 23:07:14 +0300

    refactored belongsToMany insert component for both object and array cases

--- src/resources/views/components/default/insert/belongsToMany.blade.php
+++ src/resources/views/components/default/insert/belongsToMany.blade.php
@@ -1,24 +1,27 @@
 <div class="form-group">
-    <div class="mb-4">
-        <label for="{{ $key }}[]">{{ str_replace("_", " ", $key) }}</label>
-
-        <select class="belongsToManyClass w-100" name="{{ $key }}[]" id="{{ $key }}[]" multiple="multiple">
-            @if(@$selected && $selected != null)
-                @foreach($field['data'] as $option)
-                    <option @if($selected->contains("id", $option["id"])) selected @endif value="{{ $option["id"] }}">{{ $option[$field['title']] }}</option>
-                @endforeach
-            @else
-                @foreach($field['data'] as $option)
-                    <option value="{{ $option["id"] }}">{{ $option[$field['title']] }}</option>
-                @endforeach
-            @endif
-        </select>
-        @if ($errors->has($key))
-            <div class="invalid-feedback d-block">
-                {{ $errors->first($key) }}
-            </div>
-        @endif
-    </div>
+	<div class="mb-4">
+		<label for="{{ $key }}[]">{{ str_replace("_", " ", $key) }}</label>
+		<select class="belongsToManyClass w-100" name="{{ $key }}[]" id="{{ $key }}[]" multiple="multiple">
+			@if(@$selected && $selected != null)
+				@foreach($field['data'] as $option)
+					@if(is_object($selected))
+						<option @if($selected->contains("id", $option["id"])) selected @endif value="{{ $option["id"] }}">{{ $option[$field['title']] }}</option>
+					@else
+						<option @if(in_array($option["id"], $selected)) selected @endif value="{{ $option["id"] }}">{{ $option[$field['title']] }}</option>
+					@endif
+				@endforeach
+			@else
+				@foreach($field['data'] as $option)
+					<option value="{{ $option["id"] }}">{{ $option[$field['title']] }}</option>
+				@endforeach
+			@endif
+		</select>
+		@if ($errors->has($key))
+			<div class="invalid-feedback d-block">
+				{{ $errors->first($key) }}
+			</div>
+		@endif
+	</div>
 </div>
 
 <script>
