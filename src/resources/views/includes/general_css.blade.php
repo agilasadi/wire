218df73a6e83169840b230d93991e806aaf5a46f
agilasadi <newsma.95@gmail.com>
2019-09-01 10:15:42 +0300

    general_css was updated to have true and false parameters for boolean field

--- src/resources/views/includes/general_css.blade.php
+++ src/resources/views/includes/general_css.blade.php
@@ -1,2 +1,14 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('wire-assets/css/style.css') }}">
+<style>
+	.recordStatus1{
+		background: #9ae6b4;
+		height: 0.75rem;
+		width: 0.75rem;
+	}
+	.recordStatus0{
+		background: #e53e3e;
+		height: 0.75rem;
+		width: 0.75rem;
+	}
+</style>
