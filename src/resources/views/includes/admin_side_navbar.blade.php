231e8e944bc1de8670f072a299c6b88bf908c2dc
agilasadi <newsma.95@gmail.com>
2019-09-15 12:24:47 +0300

    admin_side_navbar extra list was removed

--- src/resources/views/includes/admin_side_navbar.blade.php
+++ src/resources/views/includes/admin_side_navbar.blade.php
@@ -18,39 +18,6 @@
 				@endforeach
 			@endif
 		</ul>
-
-		<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-			<span>Accepted Projects</span>
-			<a class="d-flex align-items-center text-muted" href="#">
-				<span data-feather="plus-circle"></span>
-			</a>
-		</h6>
-		<ul class="nav flex-column mb-2">
-			<li class="nav-item">
-				<a class="nav-link" href="#">
-					<span data-feather="file-text"></span>
-					Current month
-				</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">
-					<span data-feather="file-text"></span>
-					Last quarter
-				</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">
-					<span data-feather="file-text"></span>
-					Social engagement
-				</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">
-					<span data-feather="file-text"></span>
-					Year-end sale
-				</a>
-			</li>
-		</ul>
-        @include('wire.views.includes.footer')
+		@include('wire.views.includes.footer')
 	</div>
 </nav>
