fa082c24851afe035ce6876326cbed02dc04ba5b
agilasadi <newsma.95@gmail.com>
2019-09-16 00:22:13 +0300

    Edit view was updated to fit the new design pattern

--- src/resources/views/crud/default/edit.blade.php
+++ src/resources/views/crud/default/edit.blade.php
@@ -1,19 +1,21 @@
 @extends('wire.views.layouts.master')
 
 @section('content')
+	<div class="shadow rounded bg-white">
+		@component('wire.views.includes.crud_actions', ['module' => $model])
+		@endcomponent
 
-    @component('wire.views.includes.crud_actions', ['module' => $model])
-    @endcomponent
+		<form method="post" action="{{ route('wire.update', [$model, $data->id]) }}" enctype="multipart/form-data">
+			@method('PUT')
+			@csrf
+			<div class="col-md-12 mt-3">
+				@component('wire.views.components.default.form', ['fields' => $fields,'model' => $model, 'method' => 'edit', 'data' => $data])
+				@endcomponent
+			</div>
 
-    <div class="col-md-12 mb-5">
-        <form class="mb-4" method="post" action="{{ route('wire.update', [$model, $data->id]) }}" enctype="multipart/form-data">
-            @method('PUT')
-            @csrf
-
-            @component('wire.views.components.default.form', ['fields' => $fields,'model' => $model, 'method' => 'edit', 'data' => $data])
-            @endcomponent
-
-            <button type="submit" class="btn btn-primary">{{ trans('wire::button_input.update') }}</button>
-        </form>
-    </div>
+			<div class="bg-light p-3 rounded-bottom">
+				<button type="submit" class="btn btn-primary text-uppercase font-weight-bold shadow-rg">{{ trans('wire::button_input.update') }}</button>
+			</div>
+		</form>
+	</div>
 @endsection
