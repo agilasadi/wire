8900b0c098b53305ef341247778c6b5a6efff55a
agilasadi <newsma.95@gmail.com>
2019-09-16 16:16:20 +0300

    text_editor component was removed and replaced with textarea

--- src/resources/views/components/default/insert/textarea.blade.php
+++ src/resources/views/components/default/insert/textarea.blade.php
@@ -1,28 +1,27 @@
-{{ dd($field) }}
 @section('style')
-<link href="{{ asset('wire-assets/css/external/summernote/summernote-bs4.css') }}" rel="stylesheet">
+	<link href="{{ asset('wire-assets/css/external/summernote/summernote-bs4.css') }}" rel="stylesheet">
 @endsection
 
 <div class="form-group">
-    <div class="mb-4">
-        <label for="{{ $key }}">{{ trans('wire::button_input.'.$key) }}</label>
-        <textarea id="{{ $key }}" name="{{ $key }}">{{@$record}}</textarea>
-        @if ($errors->has($key))
-            <div class="invalid-feedback d-block">
-                {{ $errors->first($key) }}
-            </div>
-        @endif
-    </div>
+	<div class="mb-4">
+		<label for="{{ $key }}">{{ str_replace("_", " ", $key) }}</label>
+		<textarea id="{{ $key }}" name="{{ $key }}">{{old($key) ? old($key) : @$record }}</textarea>
+		@if ($errors->has($key))
+			<div class="invalid-feedback d-block">
+				{{ $errors->first($key) }}
+			</div>
+		@endif
+	</div>
 </div>
 
 @section('script')
-<script src="{{ asset('wire-assets/js/external/summernote-bs4.js') }}"></script>
-<script>
-    var field_name = '{{ $key }}';
+	<script src="{{ asset('wire-assets/js/external/summernote-bs4.js') }}"></script>
+	<script>
+        var field_name = '{{ $key }}';
         $('#' + field_name).summernote({
-            placeholder: '{{ trans('wire::button_input.'.@$key) }}',
+            placeholder: '{{ str_replace("_", " ", $key) }}',
             tabsize: 2,
             height: 200
         });
-</script>
+	</script>
 @endsection
