8a58bf55a4b517ad7f560a23420788d158cf71cc
agilasadi <newsma.95@gmail.com>
2019-09-16 15:02:28 +0300

    Copy rights for svg were added to footer

--- src/resources/views/includes/footer.blade.php
+++ src/resources/views/includes/footer.blade.php
@@ -10,7 +10,8 @@
             <p>
                 Icons made by
                 <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons"> Smashicons</a>,
-                <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+                <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>,
+                <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a>
                 from
                 <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
                 is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"
