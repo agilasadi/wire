1e310c4def4318a3798a8e0b7fcedbae0e912be0
agilasadi <newsma.95@gmail.com>
2019-09-16 15:35:39 +0300

    admin navbar font awesome replacement was applied

--- src/resources/views/includes/admin_navbar.blade.php
+++ src/resources/views/includes/admin_navbar.blade.php
@@ -2,12 +2,11 @@
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('wire.dashboard') }}">{{ config('wire.name') }}</a>
     <input class="form-control form-control-dark a_n_search_input" type="text" placeholder="Search" aria-label="Search">
 
-    <div class="pl-4 pr-1">
-        <button class="navbar-toggler border-0 user_actions_slide user_actions_toggler" id="user_actions_slide">
-            <i class="fas fa-align-left"></i>
-        </button>
-        <button class="navbar-toggler border-0 d-none user_actions_close user_actions_toggler" id="user_actions_close">
-            <i class="fas fa-times"></i>
-        </button>
+    <div class="pr-3">
+        <span class="border-0 text-light user_actions_toggler button_svg_icon transparent_button cursor-pointer" id="user_actions_slide">
+            <svg class="button_svg_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+        </span>
     </div>
 </nav>
