61e445af8599d8ff63ca2e950406843a7707a70a
agilasadi <newsma.95@gmail.com>
2019-09-16 15:32:00 +0300

    general js type in asset was resolved

--- src/resources/views/includes/general_js.blade.php
+++ src/resources/views/includes/general_js.blade.php
@@ -1,6 +1,6 @@
-<script src="{{ assert('wire-assets/js/jquery-3.4.1.min') }}"></script>
-<script src="{{ assert('wire-assets/js/popper.min.js') }}"></script>
-<script src="{{ assert('wire-assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ assert('wire-assets/js/select2.min.js') }}"></script>
+<script src="{{ asset('wire-assets/js/jquery-3.4.1.min') }}"></script>
+<script src="{{ asset('wire-assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('wire-assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('wire-assets/js/select2.min.js') }}"></script>
 
 <script src="{{ asset('wire-assets/js/script.js') }}"></script>
