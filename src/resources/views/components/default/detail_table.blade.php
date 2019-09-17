48c13b327da5db287e7811334f1e26b9e579d360
agilasadi <newsma.95@gmail.com>
2019-09-17 13:03:50 +0300

    Detail table component was updated according to new design pattern

--- src/resources/views/components/default/detail_table.blade.php
+++ src/resources/views/components/default/detail_table.blade.php
@@ -1,12 +1,12 @@
-<table class="table table-striped">
+<table class="table table-borderless mb-0">
 	<tbody>
 	@if($data)
 		@foreach($fields as $field_key => $field_value)
 			@if(@$field_value['available_in'] && in_array($method, $field_value['available_in'], true))
 				<tr>
-					<th class="text-capitalize">{{ str_replace("_", " ", $field_key) }}</th>
+					<th class="text-uppercase bg-light px-4 py-3 text-dark">{{ str_replace("_", " ", $field_key) }}</th>
 
-					<td>
+					<td class="px-4 py-3 text-dark">
 						@component('wire.views.components.default.table.'.$field_value['type'],
 							[
 								'record' => $data,
@@ -18,10 +18,11 @@
 						@endcomponent
 					</td>
 				</tr>
-		    @endif
-	@endforeach
+			@endif
+		@endforeach
 	@else
-		<p class="text-center p-5 bg-light border rounded text-info h6"><i class="far fa-folder-open mr-2"></i> {{ trans('wire::wire.nothing_to_show') }}</p>
+		<p class="text-center p-5 bg-light border rounded text-info h6">
+			<i class="far fa-folder-open mr-2"></i> {{ trans('wire::wire.nothing_to_show') }}</p>
 	@endif
 	</tbody>
 </table>
