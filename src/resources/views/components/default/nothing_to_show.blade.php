491021edfdebbda6d9c01728b40860aa268d5523
agilasadi <newsma.95@gmail.com>
2019-09-17 13:22:17 +0300

    nothing_to_show component was added

---
+++ src/resources/views/components/default/nothing_to_show.blade.php
@@ -0,0 +1,10 @@
+<div class="p-2 text-center">
+	<div class="p-5 bg-light border rounded text-info h6 w-100">
+		<div class="svg_button">
+			<svg style="" class="button_svg_icon mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+				<path d="M0 4c0-1.1.9-2 2-2h7l2 2h7a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2 2v10h16V6H2z"></path>
+			</svg>
+			{{ trans('wire::wire.nothing_to_show') }}
+		</div>
+	</div>
+</div>
