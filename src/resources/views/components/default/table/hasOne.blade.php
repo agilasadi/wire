d9c192284b14b3d94d489fb7a1c730011be3161b
agilasadi <newsma.95@gmail.com>
2019-09-17 13:02:49 +0300

    hasOne view url bug was resolved

--- src/resources/views/components/default/table/hasOne.blade.php
+++ src/resources/views/components/default/table/hasOne.blade.php
@@ -1,5 +1,5 @@
 @if($record && $record->{$parameters['method']} )
-	<a href="{{ route('wire.show', ['route' => strtolower(basename($parameters["identifier"])), 'id' => $record->{$parameters['method']}->id]) }}">
+	<a href="{{ route('wire.show', ['identifier' => strtolower(basename($parameters["identifier"])), 'id' => $record->{$parameters['method']}->id]) }}">
 		{{ @$parameters['title'] ? $record->{$parameters['method']}->{$parameters['title']} : $record->{$parameters['method']}->id }}
 	</a>
 @else
