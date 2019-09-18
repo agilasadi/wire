ff5bae9ffd030ac706ab5697a6a8219adc35782c
agilasadi <newsma.95@gmail.com>
2019-09-18 16:03:27 +0300

    Resolved an issue where image would keep it's current position when the page horizontally scrolled

--- src/resources/views/components/default/table/image.blade.php
+++ src/resources/views/components/default/table/image.blade.php
@@ -1,14 +1,16 @@
-@if($record[$key] != null)
-	<img class="{{ @$class }} rounded-circle position-absolute"
-	     @if(@$parameters['disk'])
-	     src="{{asset('storage/'. @$parameters['disk'].'/'.$record[$key])}}"
-	     @else
-	     src="{{asset('storage/'.$record[$key])}}"
-	     @endif
-	     style="width: {{ @$width ? $width : '1.5rem' }}; height: {{ @$height ? $height : '1.5rem' }}" alt="wire">
-@else
-	<img class="{{ @$class }} rounded-circle position-absolute"
-	     src="{{asset('wire-assets/images/placeholder/no_image.jpg')}}"
-	     style="width: {{@$width ? $width : '1.5rem'}}; height: {{ @$height ? $height : '1.5rem' }}" alt="wire">
-@endif
-{{ $slot }}
+<div class="position-relative">
+	@if($record[$key] != null)
+		<img class="{{ @$class }} rounded-circle position-absolute"
+		     @if(@$parameters['disk'])
+		     src="{{asset('storage/'. @$parameters['disk'].'/'.$record[$key])}}"
+		     @else
+		     src="{{asset('storage/'.$record[$key])}}"
+		     @endif
+		     style="width: {{ @$width ? $width : '1.5rem' }}; height: {{ @$height ? $height : '1.5rem' }}" alt="wire">
+	@else
+		<img class="{{ @$class }} rounded-circle position-absolute"
+		     src="{{asset('wire-assets/images/placeholder/no_image.jpg')}}"
+		     style="width: {{@$width ? $width : '1.5rem'}}; height: {{ @$height ? $height : '1.5rem' }}" alt="wire">
+	@endif
+	{{ $slot }}
+</div>
