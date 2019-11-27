1ad623de43a12cfdb95d978523cb62dd2826f8d5
agilasadi <newsma.95@gmail.com>
2019-11-27 14:35:50 +0300

    BelonsToMany error where the null was throwing error has been resolved

--- src/resources/views/components/default/table/belongsToMany.blade.php
+++ src/resources/views/components/default/table/belongsToMany.blade.php
@@ -1,4 +1,4 @@
-@if($record && !$record->{$parameters['method']}->isEmpty() )
+@if($record && $record->{$parameters['method']} != null && !$record->{$parameters['method']}->isEmpty())
 	@foreach($record->{$parameters['method']} as $item)
 		<a href="{{ route('wire.show', ['identifier' => strtolower(basename($parameters["identifier"])), 'id' => $item->id]) }}">
 			{{ @$parameters['title'] ? $item->{$parameters['title']} : $item->id }}</a>{{ $loop->last ? "" : ", " }}
