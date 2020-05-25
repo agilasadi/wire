932faa46b6dda1fe3fe9149eaa66ff95584daa4b
agilasadi <newsma.95@gmail.com>
2020-05-26 02:35:54 +0300

    Wire will now support plain textarea along side textEditor fields

--- src/resources/views/components/default/insert/textarea.blade.php
+++ src/resources/views/components/default/insert/textarea.blade.php
@@ -1,27 +1,17 @@
-@section('style')
-	<link href="{{ asset('wire-assets/css/external/summernote/summernote-bs4.css') }}" rel="stylesheet">
-@endsection
-
 <div class="form-group">
-	<div class="mb-4">
-		<label for="{{ $key }}">{{ str_replace("_", " ", $key) }}</label>
-		<textarea id="{{ $key }}" name="{{ $key }}">{{old($key) ? old($key) : @$record }}</textarea>
-		@if ($errors->has($key))
-			<div class="invalid-feedback d-block">
-				{{ $errors->first($key) }}
-			</div>
-		@endif
-	</div>
-</div>
+	<label class="text-capitalize" for="{{ $key }}">{{ str_replace("_", " ", $key) }}</label>
 
-@section('script')
-	<script src="{{ asset('wire-assets/js/external/summernote-bs4.js') }}"></script>
-	<script>
-		var field_name = '{{ $key }}';
-		$('#' + field_name).summernote({
-			placeholder: '{{ str_replace("_", " ", $key) }}',
-			tabsize: 2,
-			height: 200
-		});
-	</script>
-@endsection
+	<textarea type="text"
+			  class="form-control"
+			  id="{{ $key }}"
+			  name="{{ $key }}"
+			  placeholder="{{ str_replace("_", " ", $key) }}">
+        {{ old($key) ? old($key) : @$record }}
+    </textarea>
+
+	@if ($errors->has($key))
+		<div class="invalid-feedback d-block">
+			{{ $errors->first($key) }}
+		</div>
+	@endif
+</div>
