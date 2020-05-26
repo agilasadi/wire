ccedc191439d7e1e4634eb4a08b31070f513cd97
agilasadi <newsma.95@gmail.com>
2020-05-26 12:44:00 +0300

    relational fields were updated to have class_basename instead of basename

--- src/resources/views/components/default/table/belongsToMany.blade.php
+++ src/resources/views/components/default/table/belongsToMany.blade.php
@@ -1,6 +1,6 @@
 @if($record && $record->{$parameters['method']} != null && !$record->{$parameters['method']}->isEmpty())
 	@foreach($record->{$parameters['method']} as $item)
-		<a href="{{ route('wire.show', ['identifier' => strtolower(basename($parameters["identifier"])), 'id' => $item->id]) }}">
+		<a href="{{ route('wire.show', ['identifier' => strtolower(class_basename($parameters["identifier"])), 'id' => $item->id]) }}">
 			{{ @$parameters['title'] ? $item->{$parameters['title']} : $item->id }}</a>{{ $loop->last ? "" : ", " }}
 	@endforeach
 @else
