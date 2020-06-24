776ee0e0a3cb0b7f57105b38cd58a8c7e4daace8
agilasadi <newsma.95@gmail.com>
2020-06-24 19:12:22 +0300

    side_navbar basename was replaced with class_basename

--- src/resources/views/includes/side_navbar.blade.php
+++ src/resources/views/includes/side_navbar.blade.php
@@ -10,16 +10,16 @@
 				</a>
 			</li>
 			@if(cache('identifier_classes'))
-				@foreach(cache('identifier_classes') as $class_key => $class_value)
-					<li class="nav-item">
-						<a class="nav-link d-flex inline align-items-center s_b_anchor {{ Request::is('wire/'.$class_key."*") ? 'active' : ''}}" href="{{ route('wire.index', $class_key) }}">
-							<svg style="fill: currentColor" class="s_b_icons mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-								<path d="M20 14v4a2 2 0 0 1-2 2h-4v-2a2 2 0 0 0-2-2 2 2 0 0 0-2 2v2H6a2 2 0 0 1-2-2v-4H2a2 2 0 0 1-2-2 2 2 0 0 1 2-2h2V6c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2 2 2 0 0 1 2 2v2h4a2 2 0 0 1 2 2v4h-2a2 2 0 0 0-2 2 2 2 0 0 0 2 2h2z"></path>
-							</svg>
-							{{ basename($class_value) }}
-						</a>
-					</li>
-				@endforeach
+			@foreach(cache('identifier_classes') as $class_key => $class_value)
+			<li class="nav-item">
+				<a class="nav-link d-flex inline align-items-center s_b_anchor {{ Request::is('wire/'.$class_key."*") ? 'active' : ''}}" href="{{ route('wire.index', $class_key) }}">
+					<svg style="fill: currentColor" class="s_b_icons mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+						<path d="M20 14v4a2 2 0 0 1-2 2h-4v-2a2 2 0 0 0-2-2 2 2 0 0 0-2 2v2H6a2 2 0 0 1-2-2v-4H2a2 2 0 0 1-2-2 2 2 0 0 1 2-2h2V6c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2 2 2 0 0 1 2 2v2h4a2 2 0 0 1 2 2v4h-2a2 2 0 0 0-2 2 2 2 0 0 0 2 2h2z"></path>
+					</svg>
+					{{ class_basename($class_value) }}
+				</a>
+			</li>
+			@endforeach
 			@endif
 		</ul>
 	</div>
