cf4faa18a3ee0607eb2c2e09372d49e61a75cc58
Aykut Arici <arricie@gmail.com>
2020-07-16 15:30:07 +0300

    Update date.blade.php
    carbon parser for datetime-local

--- src/resources/views/components/default/insert/date.blade.php
+++ src/resources/views/components/default/insert/date.blade.php
@@ -2,8 +2,7 @@
 	<label for="{{ $key }}">{{ str_replace("_", " ", $key) }}</label>
 
 	<input type="datetime-local" class="form-control" id="{{ $key }}" name="{{ $key }}"
-	       placeholder="{{ str_replace("_", " ", $key) }}" value="{{ old($key) ? old($key) : @$record }}">
-
+	       placeholder="{{ str_replace("_", " ", $key) }}" value="{{ old($key) ? \Carbon\Carbon::parse(old($key))->format('Y-m-d\TH:i') : \Carbon\Carbon::parse($record)->format('Y-m-d\TH:i') }}">
 	@if ($errors->has($key))
 		<div class="invalid-feedback d-block">
 			{{ $errors->first($key) }}
